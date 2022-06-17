fd4ce0f4527a8aeb7c079a45438a4dd610133d0b
Khusan <khu10735@naver.com>
2022-06-18 01:16:52 +0900

    componont

--- src/content.js
+++ src/content.js
@@ -2,20 +2,50 @@ import React from "react";
 import "./content.css"
 
 
+
+
 class Content extends React.Component{
     render() {
         return (
              <div className="content">
                 <div className="img">
-                    
+                   
                 </div>
                 <div className="malumotlar">
                     <div className="avatar">
-
+                        
                     </div>
                     <div className="title">
                         <h3>New Apartment Nice Wiew</h3>
                         <p>Quincy St, Brooklyn, NY, USA</p>
+                    </div>
+                    <div className="itemlar">
+                        <div className="data">
+                            <div>rasm</div>
+                            <p>4 Beds</p>
+                        </div>
+                        <div className="data">
+                            <div>rasm</div>
+                            <p>5 Baths</p>
+                        </div>
+                        <div className="data">
+                            <div>rasm</div>
+                            <p>1 Carage</p>
+                        </div>
+                        <div className="data">
+                            <div>rasm</div>
+                            <p>1200 Sq Ft</p>
+                        </div>
+                    </div>    
+                </div>
+                <div className="footer">
+                    <div className="pirice">
+                        <h4>$7,500/mo</h4>
+                    </div>
+                    <div className="btn">
+                        <button>+</button>
+                        <h4>1</h4>
+                        <button>-</button>
                     </div>
                 </div>
              </div>
