fe41afa6658d67f3c5d50b464d0eb7a889c6b395
Khusan <khu10735@naver.com>
2022-06-17 16:42:30 +0900

    state

---
+++ src/content.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./content.css"
+
+
+class Content extends React.Component{
+    render() {
+        return (
+             <div className="content">
+                <div className="img">
+                    
+                </div>
+                <div className="malumotlar">
+                    <div className="avatar">
+
+                    </div>
+                    <div className="title">
+                        <h3>New Apartment Nice Wiew</h3>
+                        <p>Quincy St, Brooklyn, NY, USA</p>
+                    </div>
+                </div>
+             </div>
+        );
+    }
+}
+
+export default Content
